5a4464b5295f35d1041b30564acdf3fbba06354d
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2024-08-14 15:37:56 +0900

    fix shortages

--- env/dev/state.tf
+++ env/dev/state.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "tfstate_backend" {
-  location = "asia-northeast1"
-  name     = "tfstate-backend-dev"
+  location                    = "asia-northeast1"
+  name                        = "tfstate-backend-dev"
+  uniform_bucket_level_access = true
 }
